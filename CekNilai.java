f2d3167e1cc658a3d2e2f30ca6bdd4920d9958ad
Amel <amaliamirasafitri31@gmail.com>
2025-09-09 11:15:31 +0800

    Add files via upload

---
+++ CekNilai.java
@@ -0,0 +1,11 @@
+public class CekNilai {
+    public static void main(String[] args) {
+        Perhitungan mhs1 = new Perhitungan("Iky", 90);
+        Perhitungan mhs2 = new Perhitungan("Lia", 92);
+        Perhitungan mhs3 = new Perhitungan("Kenzy", 90);
+
+        mhs1.tampilkanHasil();
+        mhs2.tampilkanHasil();
+        mhs3.tampilkanHasil();
+    }
+}
